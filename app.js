ec59b6563cce50d426e104fb5dd388c68d626d81
rajaasif <36729748+rajaasif@users.noreply.github.com>
2025-04-08 09:59:22 +0500

    error middlewares (#1)

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ import subscriptionRouter from './routes/subscription.routes.js';
 import authRouter from './routes/auth.routes.js';
 import userRouter from './routes/user.routes.js';
 import connectDB from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middlewares.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 app.use('/api/v1/subscription',subscriptionRouter);
 app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/users',userRouter);
+app.use(errorMiddleware);
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('My API Backend');
