085de3ff501c1e3c3b6cc32256762abd191711e9
rajaasif <rajaasif023@gmail.com>
2025-04-15 16:11:14 +0500

    user controller with auth

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {PORT,DB_URI} from './config/env.js';
+import { PORT, DB_URI, NODE_ENV } from './config/env.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import authRouter from './routes/auth.routes.js';
 import userRouter from './routes/user.routes.js';
@@ -11,27 +11,27 @@ const app = express();
 
 app.use(errorMiddleware);
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('My API Backend');
 });
 
-app.use('/api/v1/subscription',subscriptionRouter);
-app.use('/api/v1/auth',authRouter);
-app.use('/api/v1/users',userRouter);
+app.use('/api/v1/subscription', subscriptionRouter);
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
 
-app.listen(5500, async()=>{
-    console.log('Server is running on port',`http://localhost:${PORT}`);
-    console.log(`Server is running on ${process.env.NODE_ENV} mode`);
-    console.log(`Server is running on ${process.env.PORT} port`);
+app.listen(5500, async () => {
+    console.log('Server is running on port', `http://localhost:${PORT}`);
+    console.log(`Server is running on ${NODE_ENV} mode`);
+    console.log(`Server is running on ${PORT} port`);
     console.log(`Server is running on ${DB_URI} URI`);
     await connectDB();
 })
 export default app;
 
-// qwerty123!@# 
+// qwerty123!@#
 // asifraja023
 // user1
 // Q1yjoQGVXtDqq8o4
---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../config/env.js";
+import User from "../models/user.model.js";
+export const authorise = async (req, res, next) => {
+    try {
+        let token;
+        if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+            token = req.headers.authorization.split(" ")[1];
+        }
+        if (!token) {
+            return res.status(402).json({ message: "Not Authorised, No token provided" });
+        }
+        const decoded = jwt.verify(token, JWT_SECRET);
+        const user = await User.findById(decoded.userId);
+        if (!user) {
+            return res.status(401).json({ message: "Not Authorised, User not found" });
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        next(error);
+    }
+}
