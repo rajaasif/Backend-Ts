9cbad52451ce0b1c7f9d0dc1ffed868c86eb15ba
rajaasif <36729748+rajaasif@users.noreply.github.com>
2025-04-15 12:28:06 +0500

    JWt tokens and signup users and signin (#3)

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
+import { signUp,signIn,signOut } from '../controllers/auth.controller.js';
 const authRouter = Router();
 
-authRouter.post('/sign-up', (req,res)=>{res.send({'title': 'Sign Up'})});
-authRouter.post('/sign-in', (req,res)=>{res.send({'title': 'Sign In'})});
-authRouter.post('/sign-Out', (req,res)=>{res.send({'title': 'Sign Out'})});
+authRouter.post('/sign-up', signUp);// /api/v1/auth/sign-up
+authRouter.post('/sign-in', signIn);// /api/v1/auth/sign-in
+authRouter.post('/sign-out',signOut);// /api/v1/auth/sign-out
 
 export default authRouter;
