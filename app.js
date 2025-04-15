d5560e14ee10edd15dde69d8dca1935143db8c83
rajaasif <36729748+rajaasif@users.noreply.github.com>
2025-04-15 12:29:06 +0500

    Develop (#4)
    * pushing into develop
    
    * JWt tokens and signup users and signin (#3)

--- app.js
+++ app.js
@@ -8,9 +8,7 @@ import errorMiddleware from './middlewares/error.middlewares.js';
 import cookieParser from 'cookie-parser';
 
 const app = express();
-app.use('/api/v1/subscription',subscriptionRouter);
-app.use('/api/v1/auth',authRouter);
-app.use('/api/v1/users',userRouter);
+
 app.use(errorMiddleware);
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
@@ -19,6 +17,10 @@ app.use(cookieParser());
 app.get('/', (req, res) => {
     res.send('My API Backend');
 });
+
+app.use('/api/v1/subscription',subscriptionRouter);
+app.use('/api/v1/auth',authRouter);
+app.use('/api/v1/users',userRouter);
 
 app.listen(5500, async()=>{
     console.log('Server is running on port',`http://localhost:${PORT}`);
--- config/env.js
+++ config/env.js
@@ -5,5 +5,5 @@ import { config } from "dotenv";
 config({ path: `.env.${process.env?.NODE_ENV || "development"}.local` });
 
 // Explicitly assign variables after `config()`
-export const {PORT,NODE_ENV,DB_URI} = process.env;
+export const {PORT,NODE_ENV,DB_URI,JWT_SECRET,JWT_EXPIRES_IN} = process.env;
 
