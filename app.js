a8ce8d416fe8c8c310ad5de9c00f654760065f25
rajaasif <rajaasif023@gmail.com>
2025-04-16 10:34:40 +0500

    arcjet setup

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import userRouter from './routes/user.routes.js';
 import connectDB from './database/mongodb.js';
 import errorMiddleware from './middlewares/error.middlewares.js';
 import cookieParser from 'cookie-parser';
+import arcjetMiddleware from './middlewares/arcjet.midleware.js';
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(errorMiddleware);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(arcjetMiddleware);
 
 app.get('/', (req, res) => {
     res.send('My API Backend');
@@ -25,14 +27,6 @@ app.listen(5500, async () => {
     console.log('Server is running on port', `http://localhost:${PORT}`);
     console.log(`Server is running on ${NODE_ENV} mode`);
     console.log(`Server is running on ${PORT} port`);
-app.use('/api/v1/subscription',subscriptionRouter);
-app.use('/api/v1/auth',authRouter);
-app.use('/api/v1/users',userRouter);
-
-app.listen(5500, async()=>{
-    console.log('Server is running on port',`http://localhost:${PORT}`);
-    console.log(`Server is running on ${process.env.NODE_ENV} mode`);
-    console.log(`Server is running on ${process.env.PORT} port`);
     console.log(`Server is running on ${DB_URI} URI`);
     await connectDB();
 })
