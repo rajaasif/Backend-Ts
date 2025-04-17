9098b3c042c641918ac85103d1220c3d2e4cd01a
rajaasif <36729748+rajaasif@users.noreply.github.com>
2025-04-17 11:42:40 +0500

    workflow (#10)

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import connectDB from './database/mongodb.js';
 import errorMiddleware from './middlewares/error.middlewares.js';
 import cookieParser from 'cookie-parser';
 import arcjetMiddleware from './middlewares/arcjet.midleware.js';
+import workflowRouter from './routes/workflow.routes.js';
 
 const app = express();
 
@@ -22,6 +23,7 @@ app.get('/', (req, res) => {
 app.use('/api/v1/subscriptions', subscriptionRouter);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/workflows', workflowRouter);
 
 app.listen(5500, async () => {
     console.log('Server is running on port', `http://localhost:${PORT}`);
